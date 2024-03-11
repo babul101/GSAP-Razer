353dc1c9f47f3976b4afa9740e00d6322a96ffce
babul101 <babuldeb101@gmail.com>
2024-03-11 18:24:39 +0530

    changing the burger menu with gsap

--- js/script.js
+++ js/script.js
@@ -13,8 +13,15 @@ menuToggle.addEventListener('click',function(){
     bodyTag.classList.toggle('nav-open')
     if(bodyTag.classList.contains('nav-open')){
         menuToggleText.innerHTML = 'Close'
+        gsap.to('.burger-top',{rotation:45,transformOrigin:'50% 50%',y:8})
+        gsap.to('.burger-bottom',{rotation:-45,transformOrigin:'50% 50%',y:-8})
+        gsap.to('.burger-mid',{width:0})
     }else {
         menuToggleText.innerHTML = 'Menu'
+        gsap.to(".burger-top", { rotation: 0, y: 0 })
+        gsap.to(".burger-bottom", { rotation: 0, y: 0 })
+        gsap.to(".burger-mid", { width: 28 })
+
     }
 })
 
